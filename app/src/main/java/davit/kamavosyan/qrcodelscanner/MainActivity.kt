68387d81e828fe0da0ed6697df5aacb7fbc9855e
davit <davit.kamavosyan@gmail.com>
2021-09-29 17:02:12 +0300

    Add Qr scanner

--- app/src/main/java/davit/kamavosyan/qrcodelscanner/MainActivity.kt
+++ app/src/main/java/davit/kamavosyan/qrcodelscanner/MainActivity.kt
@@ -1,11 +1,198 @@
 package davit.kamavosyan.qrcodelscanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.Executors
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val PERMISSION_CAMERA_REQUEST = 1
+        private const val RATIO_4_3_VALUE = 4.0 / 3.0
+        private const val RATIO_16_9_VALUE = 16.0 / 9.0
+    }
+
+
+    lateinit var pvScan: androidx.camera.view.PreviewView
+    lateinit var scanResultTextView: TextView
+    private var cameraProvider: ProcessCameraProvider? = null
+    private var cameraSelector: CameraSelector? = null
+    private var lensFacing = CameraSelector.LENS_FACING_BACK
+    private var previewUseCase: Preview? = null
+    private var analysisUseCase: ImageAnalysis? = null
+    private val screenAspectRatio: Int
+        get() {
+            // Get screen metrics used to setup camera for full screen resolution
+            val metrics = DisplayMetrics().also { pvScan.display?.getRealMetrics(it) }
+            return aspectRatio(metrics.widthPixels, metrics.heightPixels)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        scanResultTextView = findViewById(R.id.scanResultTextView)
+        pvScan = findViewById(R.id.scanPreview)
+
+        setupCamera()
     }
+
+    private fun setupCamera() {
+        cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(
+            {
+                try {
+                    cameraProvider = cameraProviderFuture.get()
+                    if (isCameraPermissionGranted()) {
+                        bindCameraUseCases()
+                    } else {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            requestPermissions(
+                                arrayOf(Manifest.permission.CAMERA),
+                                PERMISSION_CAMERA_REQUEST
+                            )
+                        }
+                    }
+                } catch (e: ExecutionException) {
+                    // Handle any errors (including cancellation) here.
+                    Log.e("QrScanViewModel", "Unhandled exception", e)
+                } catch (e: InterruptedException) {
+                    Log.e("QrScanViewModel", "Unhandled exception", e)
+                }
+            },
+            ContextCompat.getMainExecutor(this)
+        )
+    }
+
+    private fun bindCameraUseCases() {
+        bindPreviewUseCase()
+        bindAnalyseUseCase()
+    }
+
+    private fun bindPreviewUseCase() {
+        if (cameraProvider == null) {
+            return
+        }
+        if (previewUseCase != null) {
+            cameraProvider?.unbind(previewUseCase)
+        }
+
+        previewUseCase = Preview.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(pvScan.display.rotation)
+            .build()
+
+        previewUseCase?.setSurfaceProvider(pvScan.surfaceProvider)
+
+        try {
+            cameraSelector?.let {
+                cameraProvider?.bindToLifecycle(this, it, previewUseCase)
+            }
+        } catch (illegalStateException: IllegalStateException) {
+        } catch (illegalArgumentException: IllegalArgumentException) {
+        }
+    }
+
+    private fun bindAnalyseUseCase() {
+        val barcodeScanner: BarcodeScanner = BarcodeScanning.getClient()
+
+        if (cameraProvider == null) {
+            return
+        }
+        if (analysisUseCase != null) {
+            cameraProvider?.unbind(analysisUseCase)
+        }
+
+        analysisUseCase = ImageAnalysis.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(pvScan.display.rotation)
+            .build()
+
+        // Initialize our background executor
+        val cameraExecutor = Executors.newSingleThreadExecutor()
+
+        analysisUseCase?.setAnalyzer(cameraExecutor, { imageProxy ->
+            processImageProxy(barcodeScanner, imageProxy)
+        })
+
+        try {
+            cameraSelector?.let {
+                cameraProvider?.bindToLifecycle(/* lifecycleOwner= */this,
+                    it, analysisUseCase
+                )
+            }
+        } catch (illegalStateException: IllegalStateException) {
+        } catch (illegalArgumentException: IllegalArgumentException) {
+        }
+    }
+
+    @SuppressLint("UnsafeExperimentalUsageError", "UnsafeOptInUsageError")
+    private fun processImageProxy(
+        barcodeScanner: BarcodeScanner,
+        imageProxy: ImageProxy
+    ) {
+        if (imageProxy.image == null) return
+        val inputImage =
+            InputImage.fromMediaImage(imageProxy.image, imageProxy.imageInfo.rotationDegrees)
+
+        barcodeScanner.process(inputImage)
+            .addOnSuccessListener { barcodes ->
+                val barcode = barcodes.getOrNull(0)
+                barcode?.rawValue?.let { code ->
+                    scanResultTextView.text = code
+
+                }
+            }
+            .addOnFailureListener {
+
+            }.addOnCompleteListener {
+                imageProxy.close()
+            }
+    }
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = max(width, height).toDouble() / min(width, height)
+        if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
+            return AspectRatio.RATIO_4_3
+        }
+        return AspectRatio.RATIO_16_9
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_CAMERA_REQUEST) {
+            if (isCameraPermissionGranted()) {
+                setupCamera()
+            } else {
+
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun isCameraPermissionGranted(): Boolean = this.let {
+        ContextCompat.checkSelfPermission(it, Manifest.permission.CAMERA)
+    } == PackageManager.PERMISSION_GRANTED
 }
